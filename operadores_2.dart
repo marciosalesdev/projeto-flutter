1fac74325591747ec63a9454472fc304f9522353
MARCIO SERRA SALES <111311224+marciosalesdev@users.noreply.github.com>
2023-08-27 00:22:46 -0300

    update

--- operadores_2.dart
+++ operadores_2.dart
@@ -12,5 +12,17 @@ a %= 2;
 
 print(a);
 
+//Operadores Atribuição (Binario/Infix) -> O resultado sempre é BOOL
+
+print(3 > 2);
+print(3 >= 3);
+print(3 < 4);
+print(3 <= 3);
+print(3 != 3);
+print(3 == 3);
+print(3 =='3');
+
+print(2 + 5 > 3 - 1 && 4 + 7 != 7 - 4);
+
 
 }
