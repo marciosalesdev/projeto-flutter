29b5fa280b15639c919f50bcc44cdf1bd6fa660f
MARCIO SERRA SALES <111311224+marciosalesdev@users.noreply.github.com>
2023-08-27 00:42:15 -0300

    incremento e decremento operador es unarios

---
+++ operadores_3.dart
@@ -0,0 +1,25 @@
+
+main(){
+//Operadores unarios)
+
+int a = 3;
+int b = 4;
+
+a++;
+--a;
+print(a);
+
+print(a++ == --b);
+print(a + b);
+
+print(!false);
+print(!true)
+;
+bool x = false;
+print(!x);
+}
+
+
+
+
+
