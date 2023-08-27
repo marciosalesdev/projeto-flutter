c929e223ee90603a3c9455d2f60c3897d2392b57
MARCIO SERRA SALES <111311224+marciosalesdev@users.noreply.github.com>
2023-08-26 23:47:30 -0300

    operadores_1

---
+++ operadores_1.dart
@@ -0,0 +1,27 @@
+main(){
+
+  int a = 7;
+  int b = 3;
+  int resultado = a + b;
+
+  print(resultado);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+  print(33 % 2);
+  print(34 % 2);
+  print(a + (b * a)  -  (b / a));
+
+  // Operadores Lógicos
+  bool  ehFragil = true;
+  bool ehCaro = false;
+
+  print(ehFragil && ehCaro);
+  print(ehFragil || ehCaro);
+  print(ehFragil ^ ehCaro);
+  print(!ehFragil);
+  print(!!ehCaro);
+  
+  
+}0
