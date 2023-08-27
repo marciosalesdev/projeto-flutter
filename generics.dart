f5d91c311d22beb72b1ff41228c3f876dcf24a16
marciosales <marcioserrasales2014@gmail.com>
2023-08-27 01:46:20 -0300

    generics

---
+++ generics.dart
@@ -0,0 +1,16 @@
+
+main(){
+
+ List<String> frutas = ['Banana', 'maça', 'Laranja'];
+frutas.add('abacate');
+
+ print(frutas);
+
+ Map<String, double> salarios = {
+  'gerente' : 1992.29,
+  'diretor' : 1298.23,
+  'estagiario' : 600.00,
+ };
+print(salarios);
+
+}
